81e9d8a9e15fe45acee8851834ae9866ac6a3d2e
Tim Joyce <tim.joyce33@gmail.com>
2017-06-30 19:37:12 -0400

    creates local json for indexing by self

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # recipe
+
+recipepull.py
+
+Allows you to search for any number of recipes using keywords. After done, recipe are written to a local file in json format.
+
+Indexing of json is in order of Label , calories, and ingredients.
--- recipepull.py
+++ recipepull.py
@@ -2,13 +2,16 @@ import json
 import requests # need this. pip install requests
 import pprint
 import sys
+import os
 
 
 # using https://developer.edamam.com/ free account for access to recipes
 
 APIkey = {'app_id': '9792fcf6','app_key': 'e3ee438479c99b54df6cba3be90a6359', 'q': ''}
 # q is name of recipe, app id and app key are api keys
-# get your own key or use mine
+
+recipeDict = {}
+data = {}
 
 
 def query(item_name):
@@ -33,10 +36,40 @@ def get_ingredients(hit_obj):
     return d
 
 
-readable = query(input())
-print('Calories: ',get_calories(readable))
-print(get_label(readable))
-print('\n'.join('{}: {}'.format(*k) for k in enumerate(get_ingredients(readable))))
+def get_recipe():
+    readable = query(input())
+    cals = get_calories(readable)
+    name = get_label(readable)
+    ingredients = get_ingredients(readable)
+    recipeDict.update({name: {cals : ingredients}})
+
+
+def write_file():
+    with open('data.json', 'w') as f:
+        json.dump(recipeDict, f, ensure_ascii=False)
+
+
+
+# def is_file_empty():
+
+# main
+print("Search for first recipe")
+get_recipe()
+print("Source more recipes? y/n")
+
+while input() is not 'n':
+    get_recipe()
+    print("Source more recipes? y/n")
+write_file()
+print("All recipes written to data.json")
+
+
+
+
+
+
+
+
 # pp = pprint.PrettyPrinter(indent=4) # make pprint object with cute organzied output
 # pp.pprint(query(input())) #type in a recipe name. returns all json related to it.
 # sys.exit()
